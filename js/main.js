3308536eeff440cfa4e582d1257e9342b9a46b85
Adamtpacz <adamtpacz@gmail.com>
2023-02-28 19:09:03 -0500

    Removed unused variables and redundant functions

--- js/main.js
+++ js/main.js
@@ -5,17 +5,6 @@ const colors = {
     '-1': 'blue'
 }
 
-const winCondition = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
 /*----- state variables -----*/
 let board
 let turn
@@ -45,14 +34,8 @@ function init() {
     // initialize state variables
     board = [null, null, null, null, null, null, null, null, null]
     turn = 1
-    winner = null
     
     // calling render function
-    render()
-}
-
-function render() {
-    console.log('rendering game')
     renderBoard()
 }
 
